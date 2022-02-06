d4939a96ebd98be9a6f69b616f003cf9a25d8f3c
Hannah Glazier <hrayglazier@aol.com>
2022-02-06 07:50:09 -0700

    commit tree cee1ba323bfdd3edaa02fda3b0d61e6c63f07509

--- index.js
+++ index.js
@@ -16,17 +16,28 @@ function moveDodgerLeft() {
     }
   });
 
-  function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left + 1}px`;
-    }
-  }
 
-  document.addEventListener("keydown", function (e) {
-      if (e.key === "ArrowRight") {
-          moveDodgerRight ();
-      }
-  });
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace('px', '')
+  const left = parseInt(leftNumbers, 10)
+
+  if (left > 0){
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function (e){
+  if (e.key === "ArrowRight"){
+    moveDodgerRight
+  }
+})
+
+
+
+
+
+
+
+
+
+
